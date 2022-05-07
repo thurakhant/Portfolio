482f7ff0dc9b5c6fd68cfe45da98ba99de2eb007
NexusHubMyanmar <thurakhantthein1500@gmail.com>
2022-05-07 13:33:01 +0630

    adding file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        btnColor: "#F1DDBF",
+        cardColor: "#F0F0F0",
+      },
+    },
+  },
+  plugins: [],
+};
