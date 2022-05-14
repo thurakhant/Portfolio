6e6853859710ddaa7c4d5b1225f64d8f927b7fa3
thurakhant <thurakhantthein4425@gmail.com>
2022-05-14 18:03:16 +0630

    adding contact session and maintain

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        primary: "#525E75",
+        secondary: "#F0F0F0",
         btnColor: "#F1DDBF",
         cardColor: "#F0F0F0",
         iconColor: "#525E75",
