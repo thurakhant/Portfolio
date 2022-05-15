4f3bd00329e413fa503efac5a62616c01cbadc9f
thurakhant <thurakhantthein4425@gmail.com>
2022-05-16 00:32:07 +0630

    adding animation and change home section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,31 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      keyframes: {
+        "spin-slow": {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        "rotate-img": {
+          "0%": { transform: "rotate(0deg)" },
+          "25%": { transform: "rotate(-90deg)" },
+          "50%": { transform: "rotate(-180deg)" },
+          "75%": { transform: "rotate(-270deg)" },
+          "100%": { transform: "rotate(-360deg)" },
+        },
+      },
+      animation: {
+        "spin-slow": "spin-slow 13s linear infinite",
+        "rotate-img": "rotate-img 13s linear infinite",
+      },
       colors: {
         primary: "#525E75",
         secondary: "#F0F0F0",
-        btnColor: "#F1DDBF",
+        btnColor: "#78938A",
         cardColor: "#F0F0F0",
         iconColor: "#525E75",
         iconAndroidColor: "#92BA92",
+        terminal: "#282c34",
       },
     },
   },
